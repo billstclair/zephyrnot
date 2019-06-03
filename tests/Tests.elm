8615959f1cd022fa84542253c138621afd0d1973
Bill St. Clair <billstclair@gmail.com>
2019-06-03 00:14:51 -0400

    Add the tests directory

---
+++ tests/Tests.elm
@@ -0,0 +1,327 @@
+module Tests exposing (all)
+
+import Dict
+import Expect exposing (Expectation)
+import Json.Decode as JD exposing (Decoder)
+import Json.Encode as JE exposing (Value)
+import List
+import Maybe exposing (withDefault)
+import Test exposing (..)
+import Zephyrnot.Board as Board
+import Zephyrnot.EncodeDecode as ED
+import Zephyrnot.Types as Types
+    exposing
+        ( Board
+        , Choice(..)
+        , GameState
+        , Message(..)
+        , Player(..)
+        , PlayerNames
+        , Score
+        , Winner(..)
+        )
+
+
+log =
+    Debug.log
+
+
+enableLogging : Bool
+enableLogging =
+    False
+
+
+
+--change to True to log JSON input & output results
+
+
+maybeLog : String -> a -> a
+maybeLog label value =
+    if enableLogging then
+        log label value
+
+    else
+        value
+
+
+testMap : (x -> String -> Test) -> List x -> List Test
+testMap test data =
+    let
+        numbers =
+            List.map Debug.toString <| List.range 1 (List.length data)
+    in
+    List.map2 test data numbers
+
+
+all : Test
+all =
+    Test.concat <|
+        List.concat
+            [ testMap protocolTest protocolData
+            , testMap boardTest boardData
+            , testMap gameStateTest gameStateData
+            ]
+
+
+expectResult : Result String thing -> Result String thing -> Expectation
+expectResult sb was =
+    case maybeLog "  result" was of
+        Err msg ->
+            case sb of
+                Err _ ->
+                    Expect.true "You shouldn't ever see this." True
+
+                Ok _ ->
+                    Expect.false msg True
+
+        Ok wasv ->
+            case sb of
+                Err _ ->
+                    Expect.false "Expected an error but didn't get one." True
+
+                Ok sbv ->
+                    Expect.equal sbv wasv
+
+
+protocolTest : Message -> String -> Test
+protocolTest message name =
+    test ("protocolTest \"" ++ name ++ "\"")
+        (\_ ->
+            let
+                pair =
+                    maybeLog "protocolJson" <| ED.messageEncoder message
+            in
+            expectResult (Ok message) <| ED.messageDecoder pair
+        )
+
+
+protocolData : List Message
+protocolData =
+    [ NewReq
+        { name = "Bill"
+        , isPublic = True
+        , restoreState = Nothing
+        }
+    , NewReq
+        { name = "Joe"
+        , isPublic = False
+        , restoreState = Just gameState1
+        }
+    , JoinReq
+        { gameid = "123"
+        , name = "Irving"
+        }
+    , JoinRsp
+        { gameid = "123"
+        , playerid = "77"
+        , names = players1
+        , gameState = gameState2
+        }
+    , LeaveReq { playerid = "77" }
+    , LeaveRsp { gameid = "123" }
+    , UpdateReq { playerid = "77" }
+    , UpdateRsp
+        { gameid = "123"
+        , gameState = gameState1
+        }
+    , PlayReq
+        { playerid = "77"
+        , placement = ChooseRow 2
+        }
+    , PlayReq
+        { playerid = "78"
+        , placement = ChooseCol 2
+        }
+    , PlayReq
+        { playerid = "79"
+        , placement = ChooseResign Zephyrus
+        }
+    , PlayReq
+        { playerid = "79"
+        , placement = ChooseResign Notus
+        }
+    , PlayReq
+        { playerid = "80"
+        , placement = ChooseNew Zephyrus
+        }
+    , PlayReq
+        { playerid = "80"
+        , placement = ChooseNew Notus
+        }
+    , PlayRsp
+        { gameid = "77"
+        , placement = ChooseRow 2
+        }
+    , PlayRsp
+        { gameid = "78"
+        , placement = ChooseCol 2
+        }
+    , PlayRsp
+        { gameid = "79"
+        , placement = ChooseResign Zephyrus
+        }
+    , PlayRsp
+        { gameid = "79"
+        , placement = ChooseResign Notus
+        }
+    , PlayRsp
+        { gameid = "80"
+        , placement = ChooseNew Zephyrus
+        }
+    , PlayRsp
+        { gameid = "80"
+        , placement = ChooseNew Notus
+        }
+    , GameOverRsp
+        { gameid = "80"
+        , winner = HorizontalWinner
+        }
+    , GameOverRsp
+        { gameid = "80"
+        , winner = VerticalWinner
+        }
+    , ErrorRsp
+        { request = "request"
+        , text = "text"
+        }
+    , ChatReq
+        { playerid = "77"
+        , text = "text"
+        }
+    , ChatRsp
+        { gameid = "123"
+        , name = "Bob"
+        , text = "text"
+        }
+    ]
+
+
+expectString : String -> String -> Expectation
+expectString sb was =
+    Expect.equal sb was
+
+
+boardTest : String -> String -> Test
+boardTest encodedBoard name =
+    test ("boardTest \"" ++ name ++ "\"")
+        (\_ ->
+            let
+                board =
+                    ED.stringToBoard encodedBoard
+
+                boardString =
+                    case board of
+                        Nothing ->
+                            ""
+
+                        Just b ->
+                            ED.boardToString b
+            in
+            expectString encodedBoard boardString
+        )
+
+
+board1String =
+    "0-----|-0----|--0---|---0--|----0-|-----0"
+
+
+board2String =
+    "----00|---00-|--00--|-00---|00----|0----0"
+
+
+decodeBoard : String -> Board
+decodeBoard string =
+    ED.stringToBoard string
+        |> Maybe.withDefault Board.empty
+
+
+board1 =
+    decodeBoard board1String
+
+
+board2 =
+    decodeBoard board2String
+
+
+boardData : List String
+boardData =
+    [ board1String
+    , board2String
+    ]
+
+
+decodeValue : Decoder a -> Value -> Result String a
+decodeValue decoder value =
+    case JD.decodeValue decoder value of
+        Ok a ->
+            Ok a
+
+        Err err ->
+            Err <| JD.errorToString err
+
+
+gameStateTest : GameState -> String -> Test
+gameStateTest gameState name =
+    test ("gameStateTest \"" ++ name ++ "\"")
+        (\_ ->
+            let
+                value =
+                    maybeLog "gameState" <| ED.encodeGameState gameState
+            in
+            expectResult (Ok gameState) <|
+                decodeValue ED.gameStateDecoder value
+        )
+
+
+players1 =
+    PlayerNames "Billy Bob" "Bobby Sue"
+
+
+players2 =
+    PlayerNames "Joe" "Random"
+
+
+score1 =
+    Score 0 1 2 3
+
+
+score2 =
+    Score 4 5 6 7
+
+
+gameState1 =
+    { board = board1
+    , moves = [ "a1", "b2", "c3" ]
+    , players = players1
+    , whoseTurn = Zephyrus
+    , score = score1
+    , winner = NoWinner
+    }
+
+
+gameState2 =
+    { board = board2
+    , moves = [ "a1", "b2", "c3", "d4" ]
+    , players = players2
+    , whoseTurn = Notus
+    , score = score1
+    , winner = HorizontalWinner
+    }
+
+
+gameState3 =
+    { board = board1
+    , moves = [ "a1", "b2", "c3", "d4", "e5", "f6" ]
+    , players = players2
+    , whoseTurn = Notus
+    , score = score2
+    , winner = VerticalWinner
+    }
+
+
+gameStateData : List GameState
+gameStateData =
+    [ gameState1
+    , gameState2
+    , gameState3
+    ]
